0be799f973522255b378ed38b3d457d5c4b67ec1
tehzwen <zachthm@gmail.com>
2019-01-24 20:36:11 -0700

    added resizing on image to load faster

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,7 +8,7 @@ class About extends Component {
 
             <div>
                 <h1 className="AboutTitle">About me</h1>
-                <img className="ZachPic" src={require('../Resources/selfie.jpg')} />
+                <img className="ZachPic" width="250" height="250" src={require('../Resources/selfie.jpg')} />
                 <br></br>
                 <text className="AboutText">My name is Zachary Shaw and I'm a full time student
                     pursuing a career in Computer Science.</text>
