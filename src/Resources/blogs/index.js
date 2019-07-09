2e7e16664b05e4498bce5e193eacce7b066d0ad0
tehzwen <zachthm@gmail.com>
2019-07-08 23:48:29 -0600

    Updates

--- src/Resources/blogs/index.js
+++
@@ -1,9 +0,0 @@
-import responsiveUpdate from './07-08-2019.json';
-import capstone from './06-29-2019.json';
-
-let blogPosts = [
-    capstone,
-    responsiveUpdate
-]
-
-export default blogPosts;
